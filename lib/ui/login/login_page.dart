4143a1810396bb8228bc3e73f2c81005cb18f211
mshaileshr@gmail.com <Yes@383840>
2020-03-01 12:18:52 +0530

    screenshots

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -7,85 +7,85 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //appBar: AppBar(centerTitle: true, elevation: 4.0, title: Text('Login', style: displayLarge(context))),
+      //appBar: AppBar(centerTitle: true, elevation: 4.0, title: Text('Login', style: displayLarge(context))),
+      //backgroundColor: Colors.blue,
 
-        body: Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-              color: Colors.grey[400],
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(250),
-                  bottomRight: Radius.circular(250))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left: 40),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.green),
-                padding: EdgeInsets.all(12),
-                child: Icon(
-                  Icons.lock,
-                  size: 45,
-                  color: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.grey[400],
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(250),
+                bottomRight: Radius.circular(250))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 40),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50), color: Colors.green),
+              padding: EdgeInsets.all(12),
+              child: Icon(
+                Icons.lock,
+                size: 45,
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40, top: 20),
+              child: Text(
+                'Welcome,',
+                style: displayXL(context),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40, top: 8),
+              child: Text(
+                'Sign In to continue',
+                style: displayXLGrey(context),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40, top: 20),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(left: 40, top: 20),
-                child: Text(
-                  'Welcome,',
-                  style: displayXL(context),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40, top: 20),
+              child: TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(left: 40, top: 8),
-                child: Text(
-                  'Sign In to continue',
-                  style: displayXLGrey(context),
-                ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              alignment: Alignment.center,
+              child: SizedBox(
+                height: 50,
+                width: 150,
+                child: RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Inventory()),
+                      );
+                    },
+                    elevation: 2.0,
+                    color: Colors.white,
+                    //splashColor: Colors.green,
+                    child: Container(child: Text('Get Started ->'))),
               ),
-              Container(
-                margin: EdgeInsets.only(left: 40, top: 20),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 40, top: 20),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  height: 50,
-                  width: 150,
-                  child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Inventory()),
-                        );
-                      },
-                      elevation: 2.0,
-                      color: Colors.white,
-                      //splashColor: Colors.green,
-                      child: Container(child: Text('Get Started ->'))),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
     //);
   }
 }
